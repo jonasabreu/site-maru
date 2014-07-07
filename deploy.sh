95347f7c08cc1abdd5bbb5ab3987ce0572d8ddd4
Jonas Abreu <jonas@vidageek.net>
2014-07-07 14:17:30 -0300

    upgrading jekyll

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
-#! /bin/bash
+#! /bin/bash --login
 
-jekyll || /var/lib/gems/1.8/bin/jekyll
+set -e
+
+source ~/.rvm/scripts/rvm
+
+rm -fr _site
+
+rvm use 2.1.0
+bundle install
+jekyll build
 
 /home/ubuntu/bin/s3-jekyll-deploy/s3-jekyll-deploy www.veterinariahomeopatia.com.br
 
+curl "http://feedburner.google.com/fb/a/pingSubmit?bloglink=http%3A%2F%2Fwwww.veterinariahomeopatia.com.br"
